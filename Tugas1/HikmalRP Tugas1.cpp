f18324b84c79d303617be5c972d781bf916e13a8
HikmalRifqiPerdana14630 <112887682+HikmalRifqiPerdana14630@users.noreply.github.com>
2022-09-05 23:00:22 +0700

    Add files via upload

---
+++ Tugas1/HikmalRP Tugas1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama           : Hikmal Rifqi Perdana" << endl;
+    cout << "Nim            : A11.2022.14630" << endl;
+    cout << "Asal Sekolah   : SMAN 1 Slawi" << endl;
+    return 0;
+}
