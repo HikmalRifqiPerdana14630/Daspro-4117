fbd0433decf29c532f7371404f90054296937e77
HikmalRifqiPerdana14630 <112887682+HikmalRifqiPerdana14630@users.noreply.github.com>
2022-09-12 21:04:28 +0700

    Add files via upload

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello!" << endl;
+
+    string nama;
+    cout << "Nama anda: ";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    cout << " selamat datang! \n" << endl;
+
+    cout << "Saya Hikmal Rifqi Perdana \nA11.2022.14630" << endl;
+
+    int u = 18;
+    float t = 165.0;
+    char b = 'L';
+
+    cout << "\nUmur Saya : " <<u;
+    cout << endl;
+    cout << "Tinggi Saya : " <<t;
+    cout << endl;
+    cout << "Ukuran Baju : " <<b;
+
+
+    return 0;
+}
