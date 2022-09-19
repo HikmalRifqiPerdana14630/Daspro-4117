2e4a7802854fa64677dd3f7f83cc4d5b63fb6f6c
HikmalRifqiPerdana14630 <112887682+HikmalRifqiPerdana14630@users.noreply.github.com>
2022-09-19 22:32:50 +0700

    Add files via upload

---
+++ Pertemuan 5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hai! \nSaya Hikmal Rifqi Perdana \nA11.2022.14630 \n4117" << endl;
+    cout << "\nDi sini saya akan membuat program untuk menghitung luas lingkaran" << endl;
+
+    float r,phi;
+    phi = 3.14;
+    cout << "\nMasukan Jari-Jari Lingkaran : ";
+    cin >>  r;
+    cout << "3.14 x r x r = " << phi*r*r << endl;
+    return 0;
+}
